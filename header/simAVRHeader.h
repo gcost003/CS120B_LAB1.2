b9d942a292bdca17fe5f600b41ef0c24781cb0b7
gcost003 <gcost003@ucr.edu>
2020-10-20 00:09:30 -0700

    Try to complete

--- header/simAVRHeader.h
+++ header/simAVRHeader.h
@@ -5,14 +5,13 @@
 #endif
 
 #include <avr/sleep.h>
-#include "include/simavr/avr/avr_mcu_section.h"
+#include "/usr/local/include/simavr/avr/avr_mcu_section.h"
 AVR_MCU(F_CPU,"atmega1284");
 AVR_MCU_VCD_FILE("build/results/Lab2_introToAVR_trace.vcd",1000);
 
 const struct avr_mmcu_vcd_trace_t _mytrace[] _MMCU_ = {
     { AVR_MCU_VCD_SYMBOL("PINA"), .what = (void*)&PINA, } ,
     { AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0,.what = (void*)&PINA, } , // Example individual pin
-    { AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, } , // Example full port
 };
 
 /* Function to output through UART */
